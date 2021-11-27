f74baa592d022b6b6917731471f3be0e1a0b8f88
mateusmacedo <mateusouza2014@live.com>
2021-11-26 22:56:45 -0300

    feat:add project struct

---
+++ src/main/java/com/base/rest/api/baserestapi/BaserestapiApplication.java
@@ -0,0 +1,14 @@
+package com.base.rest.api.baserestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaserestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaserestapiApplication.class, args);
+
+    // User user = new User(1, "Mateus", "mateusouza2014@live.com", "12345");
+	}
+}
---
+++ src/test/java/com/base/rest/api/baserestapi/BaserestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.base.rest.api.baserestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaserestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
